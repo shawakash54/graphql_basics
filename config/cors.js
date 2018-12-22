5348831f47489eda2e385957bb0efc9677cb307c
Akash Shaw <shawakash54@gmail.com>
2018-12-22 10:35:33 +0530

    cors update

--- config/cors.js
+++ config/cors.js
@@ -1,8 +1,13 @@
 import cors from 'cors'
 
+var corsOptions = {
+  origin: 'https://graphql-reactjs.herokuapp.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 //allow cross origin request
 const allowCORS = (app) => {
-  app.use(cors())
+  app.use(cors(corsOptions))
 }
 
 export default allowCORS
